e4c3cb92c346b3a2d09d8fd19bbdf116ac62c2d2
April Polubiec <aprilpolubiec@gmail.com>
2020-08-04 20:27:16 -0500

    grab music from db

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { db } from '../firebase'
 
 export default function Music() {
+  const [albums, setAlbums] = useState([])
+  useEffect(() => {
+    db.collection('albums')
+      .orderBy('date')
+      .get()
+      .then((collectionRef) => {
+        var fetchedAlbums = []
+        collectionRef.docs.forEach((doc) => {
+          fetchedAlbums.push({
+            title: doc.id,
+            url: doc.data().url,
+            id: doc.data().id,
+          })
+        })
+        setAlbums(fetchedAlbums)
+      })
+  })
+
   return (
     <div className='wrapper'>
-      Hello from music
+      <div id='music-container'>
+        {albums.map((item, index) => {
+          return (
+            <div id='placeholder'>
+              <iframe
+                src={`https://bandcamp.com/EmbeddedPlayer/album=${item.id}/size=large/bgcol=333333/linkcol=0f91ff/minimal=true/transparent=true/`}
+                seamless
+              >
+                <a href={item.url}>{item.title}</a>
+              </iframe>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
